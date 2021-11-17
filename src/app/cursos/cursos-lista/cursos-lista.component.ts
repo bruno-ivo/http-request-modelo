723687c5acd32d4f872d30477b9be6852ba311cf
bruno-ivo <73121467+bruno-ivo@users.noreply.github.com>
2021-11-17 16:58:20 -0300

    Serviços de Get do Curso

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { empty, Observable, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
 
@@ -10,13 +12,27 @@ import { CursosService } from '../cursos.service';
 })
 export class CursosListaComponent implements OnInit {
 
-  cursos?: Curso[] ;
+  //cursos?: Curso[] ;
+
+  cursos$?: Observable<Curso[]>
+  error$ = new Subject<boolean>();
 
   constructor(private cursoService: CursosService) { }
 
   ngOnInit(): void {
-    this.cursoService.list().subscribe(dados => this.cursos= dados);
+    //this.cursoService.list().subscribe(dados => this.cursos= dados);
+    this.onRefresh();
+  }
 
+  onRefresh(){
+    this.cursos$ = this.cursoService.list()
+    .pipe(
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return empty();
+      })
+    );
   }
 
 }

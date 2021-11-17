10109f5b929e0c8bc001bdf072ba2cdd7fc4df1d
bruno-ivo <73121467+bruno-ivo@users.noreply.github.com>
2021-11-17 17:14:06 -0300

    modalModule.forRoot()

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    ModalModule
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
